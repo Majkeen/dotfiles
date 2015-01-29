6a63e25659f863f12ca72c0d44d65e8b2a381939
Mikael Hallgren <hallgren.mikael@gmail.com>
2015-01-29 14:37:53 +0100

    updated syslink for Sublime Text 3

--- syslink_sublime.sh
+++ syslink_sublime.sh
@@ -1,10 +1,6 @@
 # Remove the "outdated" directories
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/
-rm -rf Packages/
-rm -rf Installed\ Packages/
-rm -rf Settings/
 
 # Then symlink your Dropbox directories back locally
 ln -s ~/Google\ Drive/Apps/Sublime/Packages/
 ln -s ~/Google\ Drive/Apps/Sublime/Installed\ Packages/
-ln -s ~/Google\ Drive/Apps/Sublime/Settings/
