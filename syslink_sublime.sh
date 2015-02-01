922ae6d6b66cb759824ece8de93bde1bccebf92b
Mikael Hallgren <hallgren.mikael@gmail.com>
2015-02-01 15:04:57 +0100

    Added .jsfmt and syslink to Atom

--- syslink_sublime.sh
+++ syslink_sublime.sh
@@ -4,3 +4,7 @@ cd ~/Library/Application\ Support/Sublime\ Text\ 3/
 # Then symlink your Dropbox directories back locally
 ln -s ~/Google\ Drive/Apps/Sublime/Packages/
 ln -s ~/Google\ Drive/Apps/Sublime/Installed\ Packages/
+
+cd ~/
+rm -r ~/.atom
+ln -s ~/Google\ Drive/Apps/Atom/.atom/
